dc5ece1708b949e991ee80b43ceafc25476aa6ce
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-09-05 13:41:25 +0530

    Add files via upload

---
+++ list_and_tuple.py
@@ -0,0 +1,23 @@
+"""In this program we will learn the difference btw tuple and and list"""
+
+# WAP to make list,
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+
+print(thislist)
+
+#now we will make changes to the list.
+thislist[2] = "chikkoo"
+thislist[3] = "Mango"
+
+print(thislist)
+
+#Lets see how tuples work
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+lovelytuple =("spain", "germany","INDIA","italy")
+print(thistuple)
+print(lovelytuple)
+
+thistuple[1] = "happy"
+
+print(thistuple)
