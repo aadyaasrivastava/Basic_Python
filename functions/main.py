94996a417681c03ea0d79ffa1a06995f85616bfc
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-09-12 11:10:40 +0530

    Add files via upload

---
+++ functions/main.py
@@ -0,0 +1,31 @@
+"""Question 1: Create a function that can accept two arguments name and age and print its value
+def demo(name, age):
+    print(name, age)
+
+demo("Juliet", 19)
+"""
+"""
+ 2: Write a function func1() such that it can accept a variable length of  argument and print all arguments value
+def func1(*args):
+    for i in args:
+        print(i)
+
+func1(20, 40, 60)
+func1(80, 100)
+"""
+
+"""
+3: def calculation(a, b):
+    return a+b, a-b
+
+res = calculation(40, 10)
+print(res)
+
+or another approach
+def calculation(a, b):
+    return a+b, a-b
+
+add, sub = calculation(40, 10)
+print(add)
+print(sub)
+"""
