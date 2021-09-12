a27b5629f543c37f4fde71e0e6d7d215d3afeb37
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-09-12 11:10:03 +0530

    creating function

---
+++ Creating function/main.py
@@ -0,0 +1,39 @@
+
+"""# Creating function
+def Python_classes():
+  print("Hello in forth lecture of this level")"""
+
+
+ """# Calling a function
+
+def my_function():
+  print("Hello from a function")
+
+
+my_function()"""
+
+"""
+  Arguments in a function
+  def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Cathy")
+  """
+"""
+  Number of arguments
+  This function expects 2 arguments, and gets 2 arguments:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+This function expects 2 arguments, but gets only 1:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
+"""
