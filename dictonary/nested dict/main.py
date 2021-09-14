4b8bb575a6bcfc71e32887fb9ce5731720a2df4d
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-09-14 15:02:24 +0530

    nested dict

---
+++ dictonary/nested dict/main.py
@@ -0,0 +1,42 @@
+#Creating a dictionary that contain three dictionaries:
+
+# myfamily = {
+#   "child1" : {
+#     "name" : "Emil",
+#     "year" : 2009
+#   },
+#   "child2" : {
+#     "name" : "Jenna",
+#     "year" : 2007
+#   },
+#   "child3" : {
+#     "name" : "Luis",
+#     "year" : 2011
+#   }
+# }
+# print(myfamily)
+
+#creating 3 dict then create one dictionary that will contain the other three dictionaries
+# child1 = {
+#   "name" : "Emil",
+#   "year" : 2004
+# }
+# child2 = {
+#   "name" : "Tobias",
+#   "year" : 2007
+# }
+# child3 = {
+#   "name" : "Linus",
+#   "year" : 2011
+# }
+#
+# myfamily = {
+#   "child1" : child1,
+#   "child2" : child2,
+#   "child3" : child3
+# }
+#
+# print(myfamily)
+
+
+#by @missc.oder
