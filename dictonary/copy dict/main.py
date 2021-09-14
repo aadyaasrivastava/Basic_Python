851cd929de1a2703026d40e652bb299f04f03229
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-09-14 15:11:21 +0530

    code to learn copying a dict

---
+++ dictonary/copy dict/main.py
@@ -0,0 +1,18 @@
+# using copy() method:
+#
+# thisdict = {
+#   "brand": "BMW",
+#   "model": "M",
+#   "year": 2021
+# }
+# mydict = thisdict.copy()
+# print(mydict)
+#Another way to make a copy is to use the built-in function dict().
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# mydict = dict(thisdict)
+# print(mydict)
