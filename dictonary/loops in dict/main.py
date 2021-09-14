022057fae359d520a05da10943d866b0d8bb07af
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2021-09-14 14:57:10 +0530

    Add files via upload

---
+++ dictonary/loops in dict/main.py
@@ -0,0 +1,50 @@
+#loops in dict
+
+
+#Print all key names in the dictionary, one by one:
+# thisdict =	{
+#   "brand": "Gucci",
+#   "Product": "Handbag",
+#   "year": 1964
+# }
+# for x in thisdict:
+#   print(x)
+
+
+#Print all values in the dictionary, one by one:
+# thisdict =	{
+#   "brand": "Gucci",
+#   "Product": "Handbag",
+#   "year": 1964
+# }
+# for x in thisdict:
+#   print(thisdict[x])
+
+
+#using the values() method to return values of a dictionary:
+# thisdict =	{
+#   "brand": "Gucci",
+#   "Product": "Handbag",
+#   "year": 1964
+# }
+# for x in thisdict.values():
+#   print(x)
+
+#using the keys() method to return the keys of a dictionary:
+# thisdict =	{
+#   "brand": "Gucci",
+#   "Product": "Handbag",
+#   "year": 1964
+#   }
+#
+# for x in thisdict.keys():
+#   print(x)
+
+#Loop through both keys and values, by using the items() method:
+# thisdict =	{
+#   "brand": "Gucci",
+#   "Product": "Handbag",
+#   "year": 1964
+# }
+# for x, y in thisdict.items():
+#   print(x, y)
---
+++ dictonary/main.py
@@ -0,0 +1,37 @@
+# #DECLARING DICT
+# thisdict = {
+#   "brand": "BMW",
+#   "model": "42 MZ",
+#   "year": 1964
+# }
+
+# #Creating and print a dictionary:
+#
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+# #printing a soecified value
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict["brand"])
+
+# #Duplicate values will overwrite existing values:
+#
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964,
+#   "year": 2020
+# }
+# print(thisdict)
+
+# #checking dict length: no of items in the dict
+#
+# print(len(thisdict))
